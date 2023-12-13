a1ca3596847f17f7168c7af47344d24d8e87a7a1
Yago Andrade <yagoandradelima@outlook.com>
2023-12-13 19:04:19 -0300

    Create tipos_primitivos.py

---
+++ treinando_sintaxes/tipos_primitivos.py
@@ -0,0 +1,14 @@
+
+# Declarando variáveis
+a = 5
+b = 3.2
+c = "Olá, Mundo!"
+d = 5 > 2
+e = [2, 'Olá']
+
+# Printando os tipos
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
