cd92c041fc01758a9674fa404f3ee3889042ca09
Yago Andrade <yagoandradelima@outlook.com>
2023-12-26 18:58:58 -0300

    Create classes.py

---
+++ treinando_sintaxes/classes.py
@@ -0,0 +1,10 @@
+class ControleRemoto:
+    def __init__(self, cor, largura, comprimento, profundidade):
+        cor = 'preto'
+        largura = '2cm'
+        comprimento = '15cm'
+        profundidade = '2cm'
+
+controle_tv = ControleRemoto('branco', '3cm', '10cm', '2cm')
+
+controle_lampada = ControleRemoto('Cinza', '4cm', '7cm', '3cm')
