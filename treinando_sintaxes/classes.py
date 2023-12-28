06633c820b893e0fcf2a04c27c28fced65c33976
Yago Andrade <100331672+yagoandradelima@users.noreply.github.com>
2023-12-27 21:52:36 -0300

    Update classes.py

--- treinando_sintaxes/classes.py
+++ treinando_sintaxes/classes.py
@@ -1,10 +1,20 @@
 class ControleRemoto:
     def __init__(self, cor, largura, comprimento, profundidade):
-        cor = 'preto'
-        largura = '2cm'
-        comprimento = '15cm'
-        profundidade = '2cm'
+        self.cor = 'preto'
+        self.largura = '2cm'
+        self.comprimento = '15cm'
+        self.profundidade = '2cm'
+
+    def passar_canal(self, botao):
+        if botao == '+':
+            print('Aumentar o canal')
+        elif botao == '-':
+            print('Diminuir o canal')
+        
 
 controle_tv = ControleRemoto('branco', '3cm', '10cm', '2cm')
-
 controle_lampada = ControleRemoto('Cinza', '4cm', '7cm', '3cm')
+
+print(f'A cor do controle da TV é {controle_tv.cor}')
+print(f'A cor do controle da lâmpada é {controle_lampada.cor}')
+controle_tv.passar_canal('+')
